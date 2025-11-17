de3c77b8b446568850b602e1c4aef208dd44dbce
Daeng <akunaink43@gmail.com>
2025-11-17 19:35:09 +0700

    Update progress.php

--- pages/progress.php
+++ pages/progress.php
@@ -541,7 +541,7 @@ $conn->close();
 
     <!-- Bottom Navigation -->
     <nav class="bottom-nav">
-        <a href="home.php" class="nav-item">
+        <a href="progress.php" class="nav-item">
             <div class="nav-icon-bg">
                 <i class="fas fa-home nav-icon"></i>
             </div>
